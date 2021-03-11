a437253621ba7920ab6f1e3d40cf3b744a84cfd4
Cody Regis <codyregis6891@yahoo.com>
2021-03-11 14:15:56 -0800

    backend

--- server.js
+++ server.js
@@ -1,61 +1,77 @@
 // Dependencies
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 // Data
-const customerInfo = {
-  name: 'Yoda',
-  role: 'Jedi Master',
-  age: 900,
-  forcePoints: 2000,
-};
-
-const darthmaul = {
-  name: 'Darth Maul',
-  role: 'Sith Lord',
-  age: 200,
-  forcePoints: 1200,
-};
-
-// Create one more data entry for the character Obi Wan Kenobi.
-const obiwanken = {
-  name: 'Obi Wan Kenobi',
-  role: 'Jedi Master',
-  age: 57,
-  forcePoints: 1220,
+const reservation = [
+  {
+  customerName: 'John',
+  customerEmail: 'j@j.com',
+  customerID: 1,
+  phoneNumber: '5555555',
+},
+{
+  customerName: 'Josh',
+  customerEmail: 'j@j.net',
+  customerID: 2,
+  phoneNumber: '5545555',
 }
-// Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
-//
+];
 
-// YOUR CODE GOES HERE
-
-//
+const waitlist = [
+{
+  customerName: 'Jim',
+  customerEmail: 'j@j.org',
+  customerID: 3,
+  phoneNumber: '4444444',
+}
+];
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('Welcome to the Star Wars Page!');
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/api/reservation', (req, res) => {
+  res.json(reservation);
 });
 
-app.get('/yoda', (req, res) => {
-  res.json(yoda);
+app.get('/api/waitlist', (req, res) => {
+  res.json(waitlist);
 });
 
-app.get('/darthmaul', (req, res) => {
-  res.json(darthmaul);
-});
 
-// Create a new Express route that leads users to the new Obi Wan Kenobi Data
-app.get('/obiwanken', (req, res) => {
-  res.json(obiwanken);
-});
-// Follow the same format as the Yoda and Darth Maul routes
-//
+if (reservation.length < 5) {
+  app.post('/api/reservation', (req, res) => {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  const newReservation = req.body;
 
-// YOUR CODE GOES HERE
-//
-//
+  console.log(newReservation);
+
+  // We then add the json the user sent to the character array
+  reservation.push(newReservation);
+
+  // We then display the JSON to the users
+  res.json(newReservation);
+});
+} else {
+  app.post('/api/waitlist', (req, res) => {
+    
+    const newWaitlist = req.body;
+  
+    console.log(newWaitlist);
+  
+    waitlist.push(newWaitlist);
+  
+    res.json(newWaitlist);
+  });
+};
 
 // Listener
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
